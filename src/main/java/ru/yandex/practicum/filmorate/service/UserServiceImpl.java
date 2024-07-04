5c03cd42ac801f995a9f2e7ef6805d9c4b6c2efc
Алексей Романов <saimonnice13@mail.ru>
2024-07-04 07:03:14 +0700

    Убрать не используемые импорты

--- src/main/java/ru/yandex/practicum/filmorate/service/UserServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserServiceImpl.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.dto.UserDto;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
@@ -13,7 +12,6 @@ import ru.yandex.practicum.filmorate.storage.FriendsRepository;
 import ru.yandex.practicum.filmorate.storage.UserRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Slf4j
