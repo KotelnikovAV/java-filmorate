74ea286defb7e147de38b31ad17ba2a4495f8404
Stanislav <stanislav.panishev@yandex.ru>
2024-07-08 13:05:12 +0600

    Add_reviews

--- src/main/java/ru/yandex/practicum/filmorate/storage/mapper/ReviewRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mapper/ReviewRowMapper.java
@@ -1,10 +1,12 @@
 package ru.yandex.practicum.filmorate.storage.mapper;
+
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Review;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 @Component
 public class ReviewRowMapper implements RowMapper<Review> {
     @Override
