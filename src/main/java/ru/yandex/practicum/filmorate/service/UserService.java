9fc45ec517fdf8ae9bcba431a7464adec094ec1d
Алексей Романов <saimonnice13@mail.ru>
2024-07-10 00:25:49 +0700

    Итоговый вариант

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -2,8 +2,6 @@ package ru.yandex.practicum.filmorate.service;
 
 import ru.yandex.practicum.filmorate.dto.FilmDto;
 import ru.yandex.practicum.filmorate.dto.UserDto;
-import ru.yandex.practicum.filmorate.dto.UserEventDto;
-import ru.yandex.practicum.filmorate.model.Film;
 
 
 import java.util.List;
@@ -28,7 +26,5 @@ public interface UserService {
 
     void delete(int id);
 
-    List<Film> getRecommendationsFilms(int userId);
-  
     List<FilmDto> getRecommendationsFilms(int userId);
 }
